b32f40238970d2d558da382aa5b976b8744468e8
Luis <solarluiso@gmail.com>
2024-12-22 11:28:23 -0500

    add mood tracking functionality to Dashboard component with Firebase integration and loading state handling

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,9 +1,78 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Calendar from "./Calendar";
+import { useAuth } from "@/context/AuthContext";
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "@/firebase";
+import Loading from "./Loading";
+import Login from "./Login";
 
 export default function Dashboard() {
+  const { currentUser, userDataObj, setUserDataObj, loading } = useAuth();
+  const [data, setData] = useState({});
+  const now = new Date();
+
+  function countValues() {
+    let total_number_of_days = 0;
+    let sum_moods = 0;
+    for (let year in data) {
+      for (let month in data[year]) {
+        for (let day in data[year][month]) {
+          let days_mood = data[year][month][day];
+          total_number_of_days++;
+          sum_moods += days_mood;
+        }
+      }
+    }
+    return {
+      num_days: total_number_of_days,
+      average_mood: sum_moods / total_number_of_days,
+    };
+  }
+
+  const statuses = {
+    ...countValues(),
+    time_remaining: `${23 - now.getHours()}H ${60 - now.getMinutes()}M`,
+  };
+
+  async function handleSetMood(mood) {
+    const day = now.getDate();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+
+    try {
+      const newData = { ...userDataObj };
+      if (!newData?.[year]) {
+        newData[year] = {};
+      }
+      if (!newData?.[year]?.[month]) {
+        newData[year][month] = {};
+      }
+
+      newData[year][month][day] = mood;
+      // update the current state
+      setData(newData);
+      // update the global state
+      setUserDataObj(newData);
+      // update firebase
+      const docRef = doc(db, "users", currentUser.uid);
+      const res = await setDoc(
+        docRef,
+        {
+          [year]: {
+            [month]: {
+              [day]: mood,
+            },
+          },
+        },
+        { merge: true }
+      );
+    } catch (err) {
+      console.log("Failed to set data: ", err.message);
+    }
+  }
+
   const moods = {
     Sad: "😢",
     Anxious: "😟",
@@ -12,11 +81,20 @@ export default function Dashboard() {
     Joyful: "😄",
   };
 
-  const statuses = {
-    num_days: 21,
-    time_remaining: "19:59:59",
-    date: new Date().toDateString(),
-  };
+  useEffect(() => {
+    if (!currentUser || !userDataObj) {
+      return;
+    }
+    setData(userDataObj);
+  }, [currentUser, userDataObj]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (!currentUser) {
+    return <Login />;
+  }
 
   return (
     <div className="flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16">
@@ -42,7 +120,10 @@ export default function Dashboard() {
         {Object.keys(moods).map((mood, moodIndex) => {
           return (
             <button
-              onClick={() => {}}
+              onClick={() => {
+                const currentMoodValue = moodIndex + 1;
+                handleSetMood(currentMoodValue);
+              }}
               className="flex flex-col items-center flex-1 gap-2 p-4 px-5 text-center duration-200 rounded-2xl purpleShadow bg-indigo-50 hover:bg-indigo-100"
               key={moodIndex}
             >
@@ -54,7 +135,7 @@ export default function Dashboard() {
           );
         })}
       </div>
-      <Calendar demo />
+      <Calendar completeData={data} handleSetMood={handleSetMood} />
     </div>
   );
 }
