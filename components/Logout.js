1331dba8fe7cc07bf5a4b35ca17582d401aab47f
Luis <solarluiso@gmail.com>
2024-12-23 09:14:14 -0500

    feat: implement Logout component with user authentication handling and conditional navigation

--- components/Logout.js
+++ components/Logout.js
@@ -1,5 +1,36 @@
+"use client";
+
 import React from "react";
+import Button from "./Button";
+import { useAuth } from "@/context/AuthContext";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Logout() {
-  return <div>Logout</div>;
+  const { logout, currentUser } = useAuth();
+  const pathname = usePathname();
+
+  const handleLogout = () => {
+    console.log("Logout button clicked");
+    try {
+      logout(); // Logout the user
+    } catch (error) {
+      console.error("Failed to logout:", error);
+    }
+  };
+
+  if (!currentUser) {
+    console.log("No current user detected");
+    return null;
+  }
+
+  const isHomePage = pathname === "/";
+
+  return isHomePage ? (
+    <Link href="/dashboard">
+      <Button text="Go to Dashboard" />
+    </Link>
+  ) : (
+    <Button text="Logout" clickHandler={handleLogout} />
+  );
 }
