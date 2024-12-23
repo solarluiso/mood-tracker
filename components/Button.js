106b4e9f2d33bb0a5a2413f779f0c2b70924dabe
Luis <solarluiso@gmail.com>
2024-12-23 09:10:47 -0500

    feat: add CallToAction component with conditional rendering for authenticated users and button navigation

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function Button(props) {
-  const { text, dark, full } = props;
+export default function Button({ text, dark, full, clickHandler }) {
   return (
     <button
+      onClick={clickHandler}
       className={
         " rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 border-solid border-indigo-600 " +
         (dark ? " text-white bg-indigo-600 " : " text-indigo-600 ") +
